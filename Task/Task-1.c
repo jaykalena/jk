584c7cb8c0ee19fafcff143280f037cb14065e4e
JAY KALENA <128819973+jaykalena@users.noreply.github.com>
2023-04-06 00:50:50 +0530

    Update Task-1.c

--- Task/Task-1.c
+++ Task/Task-1.c
@@ -1,1 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int age;
 
+    printf("Enter your age:");
+    scanf("%d", &age);
+
+   (age >= 18) ? (printf("\nYou are eligible for voting")) : (printf("\nYou are not eligible for voting "));
+}
