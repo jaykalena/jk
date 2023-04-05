7c93e412e2c4253f7d3bd5113f5f42f7e25e4bdf
JAY KALENA <128819973+jaykalena@users.noreply.github.com>
2023-04-06 00:44:04 +0530

    Update practical.c

--- Module-1/practical.c
+++ Module-1/practical.c
@@ -1,1 +1,14 @@
-
+/* Display This Information using printf
+a. Your Name
+b. Your Birth date 
+c. Your Age 
+d. Your Address
+*/
+#include<stdio.h>
+int main()
+{
+    printf("JAY KALENA");
+    printf("17th November 2003");
+    printf("19");
+    printf("RAJKOT");
+}
